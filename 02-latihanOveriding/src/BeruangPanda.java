f8a618be12bd61a2ab4cf02a1d6a414bcc2a95d7
Panandika <anandapanna@gmail.com>
2022-04-19 23:35:00 +0800

    upload 02-latihanOverriding

---
+++ 02-latihanOveriding/src/BeruangPanda.java
@@ -0,0 +1,22 @@
+public class BeruangPanda extends Beruang {
+        @Override public void habitat() {
+            System.out.println("\tHabitat\t\t\t\t= Pegunungan Tinggi Cina Barat Daya");
+        }
+        @Override
+        public void makananFavorit() {
+            System.out.println("\tMakanan Favorit\t\t= Bambu");
+        }
+        @Override
+        public void rataUsia() {
+            System.out.println("\tRata-Rata Usia\t\t= 20 tahun");
+        }
+        @Override
+        public void namaIlmiah() {
+            System.out.println("\tNama Ilmiah\t\t\t= Ailuropoda melanoleuca");
+        }
+        @Override
+        public void statusKonservasi() {
+            System.out.println("\tStatus Konservasi\t= Rentan Punah");
+        }
+}
+
